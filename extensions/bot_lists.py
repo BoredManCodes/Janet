ed79a95e3e99738766008e3a6683b46b1e41a778
LordOfPolls <ddavidallen13@gmail.com>
2022-08-26 12:16:15 +0100

    feat: post stats to top.gg

--- extensions/bot_lists.py
+++ extensions/bot_lists.py
@@ -10,11 +10,17 @@ log = logging.getLogger("bot-lists")
 class BotLists(Extension):
     def __init__(self, *args, **kwargs):
         self.discord_bots_gg_token = os.environ.get("DISCORD_BOTS_GG_TOKEN", None)
+        self.top_gg_token = os.environ.get("TOP_GG_TOKEN", None)
 
         if self.discord_bots_gg_token:
             self.discord_bots_gg.start()
         else:
             log.warning("No discord_bots_gg_token provided, not posting to discord.bots.gg")
+
+        if self.top_gg_token:
+            self.top_gg.start()
+        else:
+            log.warning("No top_gg_token provided, not posting to top.gg")
 
     @Task.create(IntervalTrigger(minutes=1))
     async def discord_bots_gg(self) -> None:
@@ -25,14 +31,28 @@ class BotLists(Extension):
                 f"https://discord.bots.gg/api/v1/bots/{self.bot.user.id}/stats",
                 json={
                     "guildCount": len(self.bot.guilds),
-                    "shardCount": self.bot.total_shards,
-                    "shardId": self.bot._connection_state.shard_id,  # noqa
                 },
             )
             if resp.status == 200:
                 log.debug("Posted stats to discord.bots.gg")
             else:
                 log.warning(f"Failed to post stats to discord.bots.gg: {resp.status} {resp.reason}")
+
+    @Task.create(IntervalTrigger(minutes=1))
+    async def top_gg(self) -> None:
+        await self.bot.wait_until_ready()
+
+        async with aiohttp.ClientSession(headers={"Authorization": self.top_gg_token}) as session:
+            resp = await session.post(
+                f"https://top.gg/api/bots/{self.bot.app.id}/stats",
+                json={
+                    "server_count": len(self.bot.guilds),
+                },
+            )
+            if resp.status == 200:
+                log.debug("Posted stats to top.gg")
+            else:
+                log.warning(f"Failed to post stats to top.gg: {resp.status} {resp.reason}")
 
 
 def setup(bot):
