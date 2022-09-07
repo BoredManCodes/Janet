aea782f7f2b4fd21cacc6267bd3393d987f52174
LordOfPolls <ddavidallen13@gmail.com>
2022-09-07 16:24:20 +0100

    feat: add details command

--- extensions/poll_utils.py
+++ extensions/poll_utils.py
@@ -1,13 +1,13 @@
 import asyncio
 import csv
-import logging
 import json
+import logging
 from io import BytesIO, StringIO
 from typing import TYPE_CHECKING
 
-import yaml
 import matplotlib.pyplot as plt
 import numpy as np
+import yaml
 from naff import (
     InteractionContext,
     SlashCommand,
@@ -15,6 +15,8 @@ from naff import (
     Timestamp,
     Permissions,
     AutocompleteContext,
+    slash_command,
+    Embed,
 )
 from thefuzz import process
 
@@ -43,6 +45,7 @@ class PollUtils(ExtensionBase):
         self.export_yaml.autocomplete("poll")(self.poll_autocomplete)
         self.export_pie.autocomplete("poll")(self.poll_autocomplete)
         self.export_bar.autocomplete("poll")(self.poll_autocomplete)
+        self.view_poll.autocomplete("poll")(self.poll_autocomplete)
 
     def get_user(self, user_id) -> str:
         try:
@@ -260,6 +263,59 @@ class PollUtils(ExtensionBase):
 
         else:
             await ctx.send("Unable to export the requested poll!")
+
+    @slash_command(
+        "details",
+        description="Display a more detailed view of a poll",
+        options=[OPT_find_poll],
+    )
+    async def view_poll(self, ctx: InteractionContext, poll) -> None:
+        if poll := await self.process_poll_option(ctx, poll):
+            await ctx.defer()
+            embed = Embed(title="Poll Details")
+            embed.color = poll.get_colour()
+
+            poll_config = []
+            if poll.expire_time:
+                poll_config.append(f"Expires {Timestamp.fromdatetime(poll.expire_time).format('R')}")
+            if poll.single_vote:
+                poll_config.append("Single vote only")
+            if poll.hide_results:
+                poll_config.append("Results hidden")
+            if poll.open_poll:
+                poll_config.append("Open poll")
+
+            embed.add_field("Question", value=poll.title, inline=True)
+            embed.add_field("Active", value="✅" if not poll.closed else "❌", inline=True)
+            embed.add_field("Author", value=f"<@{poll.author_id}>", inline=True)
+
+            if poll.description:
+                embed.add_field("Description", value=poll.description)
+
+            embed.add_field("Choices", value="\n".join(f"{opt.emoji} {opt.text}" for opt in poll.poll_options))
+
+            embed.add_field(
+                "Results",
+                value="\n".join(
+                    f"{opt.emoji} {opt.create_bar(poll.total_votes, size=24)} - {len(opt.voters):,}"
+                    for opt in poll.poll_options
+                ),
+            )
+
+            embed.add_field("Total Votes", value=f"{poll.total_votes:,}", inline=True)
+
+            embed.add_field("Configuration", value="-" + "\n-".join(poll_config or ["Default"]))
+
+            if poll.image_url:
+                embed.set_image(url=poll.image_url)
+
+            if author := await self.bot.fetch_member(poll.author_id, ctx.guild_id):
+                embed.set_thumbnail(url=author.display_avatar.url)
+
+            await ctx.send(embed=embed)
+
+        else:
+            await ctx.send("Unable to find the requested poll!")
 
 
 def setup(bot: "Bot"):
